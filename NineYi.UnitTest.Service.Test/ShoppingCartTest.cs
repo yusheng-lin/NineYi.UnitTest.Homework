d4e4ed9b758210e4c0cc82994655b7ac154cbf78
Yusheng-Lin <yushenglin@nine-yi.com>
2017-09-10 23:26:45 +0800

    0910 - add first senario test

---
+++ NineYi.UnitTest.Service.Test/ShoppingCartTest.cs
@@ -0,0 +1,29 @@
+﻿using NSubstitute;
+using System.Collections.Generic;
+using Xunit;
+
+namespace NineYi.UnitTest.Service.Test
+{
+    public class ShoppingCartTest
+    {
+        private readonly ShoppingCartService _shoppingCartService;
+
+        public ShoppingCartTest()
+        {           
+            this._shoppingCartService = new ShoppingCartService();
+        }
+
+        [Fact]
+
+        public void Test_BuyOneBook()
+        {
+            var shoppingCart = new ShoppingCartEntity { ISBNList = new string[] { "9573317249" } };
+
+            var expected = 100M;
+
+            var actual = this._shoppingCartService.Calculate(shoppingCart);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ NineYi.UnitTest.Service/ShoppingCartService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace NineYi.UnitTest.Service
+{
+    public class ShoppingCartService
+    {    
+        public decimal Calculate(ShoppingCartEntity shoppingCart)
+        {
+            if (shoppingCart == null || shoppingCart.ISBNList == null)
+                throw new ArgumentNullException("購物車不可為空");
+
+            if (shoppingCart.ISBNList.Any() == false)
+                throw new ArgumentNullException("購買書籍不可為空");
+
+            this.ProcessRule1(shoppingCart);           
+
+            return shoppingCart.Price;
+        }
+
+        /// <summary>
+        /// 一到五集的哈利波特，每一本都是賣100元
+        /// </summary>
+        /// <param name="shoppingCart"></param>
+        private void ProcessRule1(ShoppingCartEntity shoppingCart)
+        {
+            var count = shoppingCart.ISBNList.Count();
+
+            if (count > 1)
+                return;
+            shoppingCart.Price = 100;
+        }        
+    }
+}
