23c05ac7429192c42a8358539b0a5a326b57d04b
Yusheng-Lin <yushenglin@nine-yi.com>
2017-09-10 23:32:41 +0800

    0910 - add rest senario test

--- NineYi.UnitTest.Service.Test/ShoppingCartTest.cs
+++ NineYi.UnitTest.Service.Test/ShoppingCartTest.cs
@@ -9,8 +9,18 @@ namespace NineYi.UnitTest.Service.Test
         private readonly ShoppingCartService _shoppingCartService;
 
         public ShoppingCartTest()
-        {           
-            this._shoppingCartService = new ShoppingCartService();
+        {
+
+            var discountRepository = Substitute.For<IDiscountRepository>();
+            var discountMap = new Dictionary<int, decimal>
+            {
+                { 2, 0.05M },
+                { 3, 0.1M },
+                { 4, 0.2M },
+                { 5, 0.25M },
+            };
+            discountRepository.GetDiscountMap().Returns(discountMap);
+            this._shoppingCartService = new ShoppingCartService(discountRepository);
         }
 
         [Fact]
@@ -20,6 +30,66 @@ namespace NineYi.UnitTest.Service.Test
             var shoppingCart = new ShoppingCartEntity { ISBNList = new string[] { "9573317249" } };
 
             var expected = 100M;
+
+            var actual = this._shoppingCartService.Calculate(shoppingCart);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_BuyTwoDiffBook()
+        {
+            var shoppingCart = new ShoppingCartEntity { ISBNList = new string[] { "9573317249", "9573317583" } };
+
+            var expected = 2 * 100 * 0.95M;
+
+            var actual = this._shoppingCartService.Calculate(shoppingCart);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_BuyThreeDiffBook()
+        {
+            var shoppingCart = new ShoppingCartEntity { ISBNList = new string[] { "9573317249", "9573317583", "9573318008" } };
+
+            var expected = 3 * 100 * 0.9M;
+
+            var actual = this._shoppingCartService.Calculate(shoppingCart);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_BuyFourDiffBook()
+        {
+            var shoppingCart = new ShoppingCartEntity { ISBNList = new string[] { "9573317249", "9573317583", "9573318008", "9573318318" } };
+
+            var expected = 4 * 100 * 0.8M;
+
+            var actual = this._shoppingCartService.Calculate(shoppingCart);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_BuyFiveDiffBook()
+        {
+            var shoppingCart = new ShoppingCartEntity { ISBNList = new string[] { "9573317249", "9573317583", "9573318008", "9573318318", "9573319861" } };
+
+            var expected = 5 * 100 * 0.75M;
+
+            var actual = this._shoppingCartService.Calculate(shoppingCart);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_BuyThreeOutOfFourDiffeBook()
+        {
+            var shoppingCart = new ShoppingCartEntity { ISBNList = new string[] { "9573317249", "9573317583", "9573318008", "9573317249" } };
+
+            var expected = 3 * 100 * 0.9M + 100M;
 
             var actual = this._shoppingCartService.Calculate(shoppingCart);
 
--- NineYi.UnitTest.Service/ShoppingCartService.cs
+++ NineYi.UnitTest.Service/ShoppingCartService.cs
@@ -5,6 +5,13 @@ namespace NineYi.UnitTest.Service
 {
     public class ShoppingCartService
     {    
+        private readonly IDiscountRepository _discountRepository;
+
+        public ShoppingCartService(IDiscountRepository discountRepository)
+        {
+            this._discountRepository = discountRepository;
+        }
+
         public decimal Calculate(ShoppingCartEntity shoppingCart)
         {
             if (shoppingCart == null || shoppingCart.ISBNList == null)
@@ -14,14 +21,14 @@ namespace NineYi.UnitTest.Service
                 throw new ArgumentNullException("購買書籍不可為空");
 
             this.ProcessRule1(shoppingCart);           
+            this.ProcessRule2(shoppingCart);
 
             return shoppingCart.Price;
         }
 
-        /// <summary>
+        /// <remarks>
         /// 一到五集的哈利波特，每一本都是賣100元
-        /// </summary>
-        /// <param name="shoppingCart"></param>
+        /// </remarks>
         private void ProcessRule1(ShoppingCartEntity shoppingCart)
         {
             var count = shoppingCart.ISBNList.Count();
@@ -29,6 +36,36 @@ namespace NineYi.UnitTest.Service
             if (count > 1)
                 return;
             shoppingCart.Price = 100;
-        }        
+        }
+
+
+        /// <remarks>
+        /// 如果你買了三本不同的書，則會有10%的折扣
+        /// 如果你從這個系列買了兩本不同的書，則會有5%的折扣
+        /// 如果是四本不同的書，則會有20%的折扣
+        /// 如果你一次買了整套一到五集，恭喜你將享有25%的折扣
+        /// 需要留意的是，如果你買了四本書，其中三本不同，第四本則是重複的，那麼那三本將享有10%的折扣，但重複的那一本，則仍須100元。
+        /// </remarks>
+        private void ProcessRule2(ShoppingCartEntity shoppingCart)
+        {
+            var bookList = shoppingCart.ISBNList;
+
+            if (bookList.Count() < 2)
+                return;
+
+            var totalCount = bookList.Count();
+
+            var distinctCount = bookList.Distinct().Count();
+
+            var discountMap = this._discountRepository.GetDiscountMap();
+
+            var discount = discountMap[distinctCount];
+
+            var discountPrice = distinctCount * 100 * (1 - discount);
+
+            var restPrice = 100M * (totalCount - distinctCount);
+
+            shoppingCart.Price = discountPrice + restPrice;
+        }
     }
 }
